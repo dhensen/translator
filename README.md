9ff0fad62d20a796973cd465559d574018c7657b
dhensen <dino.hensen@gmail.com>
2015-04-15 12:00:30 +0200

    Added google unofficial translator

--- README.md
+++ README.md
@@ -11,10 +11,18 @@ composer require dhensen/translator
 
 ## Usage
 
+Bing/Azure:
 ```php
     $browser           = new \Buzz\Browser();
     $azureClientId     = 'your_microsoft_azure_client_id';
     $azureClientSecret = 'your_microsoft_azure_client_secret';
     $translator        = new \DHensen\Translator\Bing\BingTranslator($browser, $azureClientId, $azureClientSecret);
     echo $translator->translate('nl', 'en', 'Ik eet soep'); // prints: I eat soup
+```
+
+Google unofficial:
+```php
+$browser = new \Buzz\Browser();
+$translator = new \DHensen\Translator\Google\GoogleTranslatorUnofficial($browser);
+echo $translator->translate('nl', 'en', 'Wat ben jij aan het doen?'); // prints: What are you doing?
 ```
---
+++ src/DHensen/Translator/Google/GoogleTranslatorUnofficial.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DHensen\Translator\Google;
+
+use Buzz\Browser;
+use DHensen\Translator\Translator;
+
+class GoogleTranslatorUnofficial implements Translator
+{
+    const ENDPOINT_MASK = 'https://translate.googleapis.com/translate_a/single?client=gtx&sl=%s&tl=%s&dt=t&q=%s';
+
+    public function __construct(Browser $browser)
+    {
+        $this->browser      = $browser;
+    }
+
+    /**
+     * @param string $sourceLanguage
+     * @param string $destinationLanguage
+     * @param string $text
+     * @return string
+     */
+    public function translate($sourceLanguage, $destinationLanguage, $text)
+    {
+        $url = sprintf(self::ENDPOINT_MASK, $sourceLanguage, $destinationLanguage, urlencode($text));
+
+        $response = $this->browser->get($url);
+
+        $brokenJson = $response->getContent();
+
+        $fixedJson = str_replace(',,', ',', $brokenJson);
+
+        $data = json_decode($fixedJson);
+
+        return $data[0][0][0];
+    }
+}
